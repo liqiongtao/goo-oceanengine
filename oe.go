aabf4a8de55ec4f6fa1a70f20227b88ca95ca130
liqiongtao <hnatao@126.com>
2022-04-15 23:08:41 +0800

    bufix

--- oe.go
+++ oe.go
@@ -2,6 +2,7 @@ package goo_oceanengine
 
 import (
 	"encoding/json"
+	"errors"
 	goo_http_request "github.com/liqiongtao/googo.io/goo-http-request"
 	goo_log "github.com/liqiongtao/googo.io/goo-log"
 	"time"
@@ -40,6 +41,16 @@ func (oe oceanengine) request(method, url string, data []byte, rst interface{}, 
 	if err != nil {
 		return
 	}
+
+	var rsp map[string]interface{}
+	if err = json.Unmarshal(b, &rsp); err != nil {
+		return
+	}
+	if rsp["code"].(int) > 0 {
+		err = errors.New(rsp["message"].(string))
+		return
+	}
+
 	if err = json.Unmarshal(b, &rst); err != nil {
 		return
 	}
@@ -50,7 +61,7 @@ func (oe oceanengine) request(method, url string, data []byte, rst interface{}, 
 func (oe oceanengine) get(url string, data []byte, rst interface{}, opts ...goo_http_request.Option) (err error) {
 	for i := 0; i < 5; i++ {
 		if err = oe.request("GET", url, data, rst, opts...); err != nil {
-			time.Sleep(time.Duration(i+1) * time.Second)
+			time.Sleep(time.Second)
 			goo_log.ErrorF("重试第%d次, %s, %s", i+1, url, string(data))
 			continue
 		}
@@ -62,7 +73,7 @@ func (oe oceanengine) get(url string, data []byte, rst interface{}, opts ...goo_
 func (oe oceanengine) post(url string, data []byte, rst interface{}, opts ...goo_http_request.Option) (err error) {
 	for i := 0; i < 5; i++ {
 		if err = oe.request("POST", url, data, rst, opts...); err != nil {
-			time.Sleep(time.Duration(i+1) * time.Second)
+			time.Sleep(time.Second)
 			goo_log.ErrorF("重试第%d次, %s, %s", i+1, url, string(data))
 			continue
 		}
